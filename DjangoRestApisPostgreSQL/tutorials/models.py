ed2f01d66d1fef6f96625ec80f9747886b9dbe18
Essy Mwangi <sonnieessy@gmail.com>
2020-06-10 17:43:12 +0300

    craeted app

---
+++ DjangoRestApisPostgreSQL/tutorials/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+import datetime as dt
+
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    published = models.BooleanField(default=False)
+    content = models.TextField(max_length=500,blank=False, default='')
+    modified_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', default='default.png')
+    
+
---
+++ DjangoRestApisPostgreSQL/tutorials/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers 
+from tutorials.models import Tutorial
+ 
+ 
+class TutorialSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = Tutorial
+        fields = ('id',
+                  'title',
+                  'description',
+                  'published',
+                  'content',
+                  'modified_date',
+                  'image',
+                  'user')
+                
